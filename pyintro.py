ac847ad75c72a00e6f4500395b435754c5d19d01
Meghana <Meghana>
2016-08-06 16:27:16 +0530

    first commit

---
+++ pyintro.py
@@ -0,0 +1,40 @@
+#name='hi'
+#if name=='hi':
+#	print("hey")
+#elif name=='kay':
+#	print("hello")
+#else :
+#	print("bye")
+
+#volume = 57
+#if volume<50:
+#	print("too low")
+#elif volume<60:
+#	print("accurate")
+#elif volume<100:
+#	print("too high")
+
+#def hi():
+#	print('hi')
+#	print('hello')
+#	name='hi'
+#	if name=='hi':
+#		print("hey")
+#	elif name=='kay':
+#		print("hello")
+#	else :
+#		print("bye")
+#hi()
+
+#def hi(name):
+#	print('hi'+name+'!')
+#hi("rachel")
+
+#	print('hi'+ name+'!')
+#girls=['hi','hello']
+#for name in girls:
+#	hi(name)
+#	print('next girl')
+
+for i in range(1,6):
+	print(i)
